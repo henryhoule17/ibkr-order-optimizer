5e9aae9a5ba4dd7e955987ffe3790a8996e165ca
Henry Houle <henryhoule17@gmail.com>
2024-09-05 11:06:27 -0700

    Updated to use ib_async and generally made it more robust

--- app.py
+++ app.py
@@ -1,36 +1,134 @@
-from ib_interface import create_orders
+from concurrent.futures import ThreadPoolExecutor
+import math
 import streamlit as st
+import random
+import time
+import asyncio
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+from ib_async import Contract, Order, IB, Stock
 
-# Streamlit UI for Contract and Order details
+ORDER_SEGMENTS = 10
+CLIENT_ID = 4
 
-st.title("Contract and Order Form")
+if 'validated' not in st.session_state:
+    st.session_state.validated = False
 
-# Form for Contract details
-st.header("Contract Details")
-contract_symbol = st.text_input("Symbol", "TSM")
-contract_secType = st.selectbox("Security Type", ["STK", "OPT", "FUT", "CASH", "IND", "CFD", "BOND", "FOP", "WAR", "IOPT", "FUND", "CMDTY", "NEWS", "SPR", "BAG", "KNOWS", "TBOND", "TBILL"])
-contract_exchange = st.text_input("Exchange", "SMART")
-contract_currency = st.text_input("Currency", "USD")
+def create_orders(
+    symbol: str, 
+    secType: str, 
+    exchange: str, 
+    currency: str, 
+    action: str, 
+    totalQuantity: int, 
+    orderType: str, 
+    highestPrice: float,
+    lowestPrice: float
+):
+    ib = IB()
+    ib.connect('127.0.0.1', 7497, clientId=CLIENT_ID)
 
-# Form for Order details
-st.header("Order Details")
-order_action = st.selectbox("Action", ["BUY", "SELL"])
-order_totalQuantity = st.number_input("Total Quantity", value=10, step=1)
-order_orderType = st.selectbox("Order Type", ["LMT", "MKT", "STP", "STP LMT", "TRAIL", "TRAIL LMT"])
-order_highestPrice = st.number_input("Highest Price", value=160.90, step=0.01)
-order_lowestPrice = st.number_input("Lowest Price", value=160.90, step=0.01)
+    contract = Contract()
+    contract.symbol = symbol
+    contract.secType = secType
+    contract.exchange = exchange
+    contract.currency = currency
 
-# Submit button
-if st.button("Submit"):
-    create_orders(
-        symbol=contract_symbol, 
-        secType=contract_secType, 
-        exchange=contract_exchange, 
-        currency=contract_currency, 
-        action=order_action, 
-        totalQuantity=order_totalQuantity, 
-        orderType=order_orderType, 
-        highestPrice=order_highestPrice,
-        lowestPrice = order_lowestPrice
-    )
+    orders = []
+
+    for _ in range(ORDER_SEGMENTS):
+        diff = highestPrice - lowestPrice
+        multiple = random.random()
+        lmtPrice = lowestPrice + diff * multiple
+
+        order = Order()
+        order.action = action
+        order.totalQuantity = round(totalQuantity / ORDER_SEGMENTS)
+        order.orderType = orderType
+        order.lmtPrice = round(lmtPrice, 2)
+        orders.append(order)
+
+    time.sleep(1)
+    for o in orders:
+        ib.placeOrder(contract, o)
+    time.sleep(5)
+    ib.disconnect()
+
+def run_async_task(coroutine):
+    loop = asyncio.get_event_loop()
+    future = asyncio.run_coroutine_threadsafe(coroutine, loop)
+    return future.result()
+
+def main():
+    # Streamlit UI for Contract and Order details
+    st.title("Contract and Order Form")
+
+    # Form for Contract details
+    st.header("Contract Details")
+    contract_symbol = st.text_input("Symbol", "AAPL")
+    contract_secType = st.selectbox("Security Type", ["STK", "OPT", "FUT", "CASH", "IND", "CFD", "BOND", "FOP", "WAR", "IOPT", "FUND", "CMDTY", "NEWS", "SPR", "BAG", "KNOWS", "TBOND", "TBILL"])
+    contract_exchange = st.text_input("Exchange", "SMART")
+    contract_currency = st.text_input("Currency", "USD")
+
+    if st.button("Validate"):
+        ib = IB()
+        try:
+            ib.connect('127.0.0.1', 7497, clientId=CLIENT_ID, timeout=10)
+        except RuntimeError as re:
+            st.error("Connection to TWS failed, try restarting both this app and TWS.")
+            return
+        st.session_state.validated = True
+        print("Connected to TWS")
+        stock = Stock(contract_symbol, contract_exchange, contract_currency)
+        ib.qualifyContracts(stock)
+        print("Contract Qualified")
+        ticker = ib.reqMktData(stock, "", False, False)
+        print("Ticker Requested")
+        time.sleep(2)
+        print("Ticker Type: ", type(ticker))
+        print("Bid: ", ticker.bid)
+        print("Bid Type: ", type(ticker.bid))  
+        print("Ask: ", ticker.ask)
+        print("Next: ", ticker.close)
+        st.session_state.ticker = ticker
+        ib.disconnect()
+
+    if st.session_state.validated:
+        st.header("Order Details")
+        ticker = st.session_state.ticker
+        order_action = st.selectbox("Action", ["BUY", "SELL"])
+        if ticker and ticker.bid and ticker.ask and not math.isnan(ticker.bid) and not math.isnan(ticker.ask):
+            if order_action == "BUY":
+                orders_range = st.slider("Range", value=[ticker.bid*0.95, ticker.bid], step=0.01)
+            elif order_action == "SELL":
+                orders_range = st.slider("Range", value=[ticker.ask, ticker.ask*1.05], step=0.01)
+        else:
+            order_highestPrice = st.number_input("Highest Price", step=0.01, value=None)
+            order_lowestPrice = st.number_input("Lowest Price", step=0.01, value=None)
+            orders_range = (order_lowestPrice, order_highestPrice)
+
+        order_totalQuantity = st.number_input("Total Quantity", value=100, step=1)
+        order_type = "LMT"
+
+        # Submit button
+        if st.button("Submit"):
+            print("Contract Details:")
+            create_orders(
+                symbol=contract_symbol, 
+                secType=contract_secType, 
+                exchange=contract_exchange, 
+                currency=contract_currency, 
+                action=order_action, 
+                totalQuantity=order_totalQuantity, 
+                orderType=order_type, 
+                highestPrice=orders_range[1],
+                lowestPrice = orders_range[0]
+            )
+        if st.button("Reset"):
+            st.session_state.validated = False
+            st.session_state.ticker = None
+            st.rerun()
+
+
+main()
 
--- ib_interface.py
+++ ib_interface.py
@@ -1,21 +1,15 @@
-from curses import wrapper
 import logging
 import random
 import time
-from ibapi.client import EClient
-from ibapi.wrapper import EWrapper
-from ibapi.contract import Contract
-from ibapi.order import Order
-from ibapi.common import *
-
-from ibapi.client import EClient
-from ibapi.wrapper import EWrapper
-
-from threading import Thread
+import asyncio
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+from ib_async import Contract, Order, IB
 
 ORDER_SEGMENTS = 10
 
-def create_orders(
+async def create_orders(
+    ib: IB, 
     symbol: str, 
     secType: str, 
     exchange: str, 
@@ -26,6 +20,8 @@ def create_orders(
     highestPrice: float,
     lowestPrice: float
 ):
+    
+
     contract = Contract()
     contract.symbol = symbol
     contract.secType = secType
@@ -46,47 +42,44 @@ def create_orders(
         order.lmtPrice = round(lmtPrice, 2)
         orders.append(order)
 
-    client = IBClient('127.0.0.1', 7497, 2)
-    client.nextValidId(-1)
-
     time.sleep(1)
     for i, o in enumerate(orders):
-        print("Next ID: ", client.nextOrderId())
-        client.placeOrder(client.nextOrderId(), contract, o)
+        # print("Next ID: ", client.nextOrderId())
+        ib.placeOrder(contract, o)
         # client.openOrder(i+140, contract, o, orderState=None)
     time.sleep(5)
-    client.disconnect()
+    ib.disconnect()
 
 
-class IBClient(EWrapper, EClient):
+# class IBClient(EWrapper, EClient):
     
-    nextValidOrderId = None
+#     nextValidOrderId = None
 
-    def __init__(self, host, port, client_id):
-        EClient.__init__(self, self) 
+#     def __init__(self, host, port, client_id):
+#         EClient.__init__(self, self) 
         
-        self.connect(host, port, client_id)
+#         self.connect(host, port, client_id)
 
-        thread = Thread(target=self.run)
-        thread.start()
-
-
-    def error(self, req_id, code, msg, misc=None):
-        if code in [2104, 2106, 2158]:
-            print(msg)
-        else:
-            print('Error {}: {}'.format(code, msg))
+#         thread = Thread(target=self.run)
+#         thread.start()
 
 
-    def historicalData(self, req_id, bar):
-        print(bar)
+#     def error(self, req_id, code, msg, misc=None):
+#         if code in [2104, 2106, 2158]:
+#             print(msg)
+#         else:
+#             print('Error {}: {}'.format(code, msg))
 
 
-    # callback when all historical data has been received
-    def historicalDataEnd(self, reqId, start, end):
-        print(f"end of data {start} {end}")
+#     def historicalData(self, req_id, bar):
+#         print(bar)
 
-    def nextOrderId(self):
-        oid = self.nextValidOrderId
-        self.nextValidOrderId += 1
-        return oid
+
+#     # callback when all historical data has been received
+#     def historicalDataEnd(self, reqId, start, end):
+#         print(f"end of data {start} {end}")
+
+#     def nextOrderId(self):
+#         oid = self.nextValidOrderId
+#         self.nextValidOrderId += 1
+#         return oid
